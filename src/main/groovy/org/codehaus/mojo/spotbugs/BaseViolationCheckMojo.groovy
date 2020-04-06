47089a896e417d207e414b1bb9965c8dc4b4fac1
Jeremy Landis <jeremylandis@hotmail.com>
2020-04-05 20:58:41 -0400

    [enhance] Add definition of addSourceDirs to check mojo

--- src/main/groovy/org/codehaus/mojo/spotbugs/BaseViolationCheckMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/BaseViolationCheckMojo.groovy
@@ -100,6 +100,10 @@ abstract class BaseViolationCheckMojo extends AbstractMojo implements SpotBugsPl
     @Parameter(defaultValue = 'false', property = 'spotbugs.includeTests')
     boolean includeTests
 
+    /** Run Spotbugs with -sourcepath parameter populated with the known source roots. */
+    @Parameter(defaultValue = "false", property = "spotbugs.addSourceDirs")
+    boolean addSourceDirs
+
     /** List of artifacts this plugin depends on. Used for resolving the Spotbugs core plugin. */
     @Parameter(property = 'plugin.artifacts', readonly = true, required = true)
     List pluginArtifacts
