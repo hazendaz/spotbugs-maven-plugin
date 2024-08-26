5aedc3c73780ca47ff631f99f1165d59c87fe603
Jeremy Landis <jeremylandis@hotmail.com>
2024-08-26 18:15:27 -0400

    Revert "[cleanup] Remove items duplicated from parent in spot bugs mojo for no real reason"
    This reverts commit 1c0abecc73775d645af2e3481ff36d0a22f9aceb.

--- src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
+++ src/main/groovy/org/codehaus/mojo/spotbugs/SpotBugsMojo.groovy
@@ -22,6 +22,8 @@ import groovy.xml.XmlSlurper
 import groovy.xml.slurpersupport.GPathResult
 import groovy.xml.StreamingMarkupBuilder
 
+import org.apache.maven.doxia.siterenderer.Renderer
+import org.apache.maven.doxia.tools.SiteTool
 import org.apache.maven.execution.MavenSession
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugins.annotations.Mojo
@@ -135,6 +137,10 @@ class SpotBugsMojo extends AbstractMavenReport implements SpotBugsPluginsTrait {
      */
     @Parameter(defaultValue = 'spotbugsXml.xml', property = 'spotbugs.outputXmlFilename')
     String spotbugsXmlOutputFilename
+
+    /** Doxia Site Renderer. */
+    @Inject
+    Renderer siteRenderer
 
     /** Directory containing the class files for Spotbugs to analyze. */
     @Parameter(defaultValue = '${project.build.outputDirectory}', required = true)
@@ -414,6 +420,14 @@ class SpotBugsMojo extends AbstractMavenReport implements SpotBugsPluginsTrait {
      */
     @Inject
     ResourceManager resourceManager
+
+    /**
+     * SiteTool.
+     *
+     * @since 2.1-SNAPSHOT
+     */
+    @Inject
+    SiteTool siteTool
 
     /**
      * Fail the build on an error.
@@ -715,6 +729,15 @@ class SpotBugsMojo extends AbstractMavenReport implements SpotBugsPluginsTrait {
     @Override
     protected String getOutputDirectory() {
         return outputDirectory.getAbsolutePath()
+    }
+
+    /**
+     * Return the Site Renderer.
+     *
+     */
+    @Override
+    protected Renderer getSiteRenderer() {
+        return this.siteRenderer
     }
 
     /**
